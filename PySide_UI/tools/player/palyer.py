c932d8dcc365f5a410a6cea0bf8693055d3f76c3
chister <1511486372@qq.com>
2025-05-31 21:46:37 +0800

    增加振幅、分贝计算

--- PySide_UI/tools/player/palyer.py
+++ PySide_UI/tools/player/palyer.py
@@ -40,7 +40,6 @@ class MusicPlayer(QMediaPlayer):
         else:
             self.device_description = device.description()
 
-        print(device)
 
         # 强制释放旧设备
         if self.audio_output:
